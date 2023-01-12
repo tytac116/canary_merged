ff2fc17d9d6c4625d3710c4812b9ede514f55449
Takura <takutadi1@gmail.com>
2023-01-12 15:41:11 +0200

    combined setup

--- setup.py
+++ setup.py
@@ -75,3 +75,37 @@ setup(
         "License :: OSI Approved :: BSD License",
     ],
 )
+
+setup(
+    name='opencanary-correlator',
+    version=__version__,
+    url='http://www.thinkst.com/',
+    author='Thinkst Applied Research',
+    author_email='info@thinkst.com',
+    description='opencanary correlator',
+    install_requires=[
+        "simplejson",
+        "cffi",
+        "docopt",
+        "httplib2",
+        "mandrill",
+        "pycparser",
+        "PyNaCl",
+        "pytz",
+        "redis",
+        "requests",
+        "six",
+        "twilio",
+        "Twisted",
+        "wheel",
+        "zope.interface"
+    ],
+    setup_requires=[
+        'setuptools_git'
+    ],
+    license='BSD',
+    packages = find_packages(exclude="test"),
+    scripts=['bin/opencanary-correlator'],
+    platforms='any',
+    include_package_data=True,
+)
