873408b0dbbbd58e75696d9a5a06172b3ac00aae
Takura <takutadi1@gmail.com>
2023-01-12 16:04:46 +0200

    uncommit combination changes

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import codecs
 import os.path
 from setuptools import setup, find_namespace_packages
 import sys
-from setuptools import setup, find_packages
-from opencanary_correlator import __version__
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
@@ -75,38 +73,4 @@ setup(
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: BSD License",
     ],
-)
-
-setup(
-    name='opencanary-correlator',
-    version=__version__,
-    url='http://www.thinkst.com/',
-    author='Thinkst Applied Research',
-    author_email='info@thinkst.com',
-    description='opencanary correlator',
-    install_requires=[
-        "simplejson",
-        "cffi",
-        "docopt",
-        "httplib2",
-        "mandrill",
-        "pycparser",
-        "PyNaCl",
-        "pytz",
-        "redis",
-        "requests",
-        "six",
-        "twilio",
-        "Twisted",
-        "wheel",
-        "zope.interface"
-    ],
-    setup_requires=[
-        'setuptools_git'
-    ],
-    license='BSD',
-    packages = find_packages(exclude="test"),
-    scripts=['bin/opencanary-correlator'],
-    platforms='any',
-    include_package_data=True,
 )
